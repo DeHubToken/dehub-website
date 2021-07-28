e2de6af225410a7a0ec95521ed81828f5497711d
Aleksandr Pasevin <pasevin@gmail.com>
2021-07-28 14:37:14 +0300

    Wallet distribution responsive fixes

--- custom/js/script.js
+++ custom/js/script.js
@@ -10,43 +10,32 @@ function MY_scripts() {
 	let pieWidth = 800;
 
 	/* ------------------------------- Screen Size ------------------------------ */
+	let debounce = undefined;
 	const screen = new ScreenSizeDetector();
 	screen.setMainCallback('widthchange', () => {
-		resizePie(screen.width);
+		clearTimeout(debounce);
+		debounce = setTimeout(() => {
+			resizePie(screen.width);
+		}, 500);
 	});
 
 	/* -------------------------------- Piechart -------------------------------- */
 
-	function resizePie(screenWidth) {
-		const props = {
-			canvasHeight: 700,
-			canvasWidth: 800,
-			pieInnerRadius: '44%',
-			pieOuterRadius: '66%',
-		};
-		if (screenWidth <= 1240) {
-			props.canvasHeight = 500;
-			canvasWidth = 600;
-			pie.updateProp('size', props);
-		} else if (screenWidth > 1240) {
-			props.canvasHeight = 700;
-			canvasWidth = 800;
-			pie.updateProp('size', props);
-		}
-	}
-
-	const pie = new d3pie('pieChart', {
+	let pieProps = {
 		header: {
 			title: {
 				fontSize: 22,
 				font: 'verdana',
+				text: '',
+				color: '#ffffff',
 			},
 			subtitle: {
-				color: '#999999',
-				fontSize: 10,
+				color: '#ffffff',
+				fontSize: 18,
 				font: 'verdana',
 			},
-			titleSubtitlePadding: 12,
+			titleSubtitlePadding: 5,
+			location: 'pie-center',
 		},
 		footer: {
 			color: '#999999',
@@ -57,11 +46,16 @@ function MY_scripts() {
 		size: {
 			canvasHeight: 700,
 			canvasWidth: 800,
-			pieInnerRadius: '44%',
+			pieInnerRadius: '64%',
 			pieOuterRadius: '66%',
 		},
 		data: {
 			content: [
+				{
+					label: 'Team',
+					value: 10,
+					color: '#62C8CE',
+				},
 				{
 					label: 'Airdrop',
 					value: 18,
@@ -97,17 +91,12 @@ function MY_scripts() {
 					value: 10,
 					color: '#8FCFD4',
 				},
-				{
-					label: 'Team',
-					value: 10,
-					color: '#62C8CE',
-				},
 			],
 		},
 		labels: {
 			outer: {
 				format: 'label-percentage2',
-				pieDistance: 25,
+				pieDistance: 55,
 			},
 			inner: {
 				format: 'none',
@@ -153,8 +142,52 @@ function MY_scripts() {
 				size: 8,
 			},
 		},
-	});
+	};
+	let pie;
 
+	function resizePie(screenWidth) {
+		pie ? pie.destroy() : undefined;
+		if (screenWidth <= 991 && screenWidth > 767) {
+			pieProps.size.canvasHeight = 650;
+			pieProps.size.canvasWidth = 750;
+			pieProps.labels.mainLabel.fontSize = 12;
+			pieProps.labels.percentage.fontSize = 10;
+			pieProps.labels.outer.pieDistance = 55;
+			pie = new d3pie('pieChart', pieProps);
+		} else if (screenWidth <= 767 && screenWidth > 575) {
+			pieProps.size.canvasHeight = 450;
+			pieProps.size.canvasWidth = 550;
+			pieProps.labels.mainLabel.fontSize = 12;
+			pieProps.labels.percentage.fontSize = 10;
+			pieProps.labels.outer.pieDistance = 55;
+			pie = new d3pie('pieChart', pieProps);
+		} else if (screenWidth <= 575 && screenWidth > 420) {
+			pieProps.size.canvasHeight = 300;
+			pieProps.size.canvasWidth = 400;
+			pieProps.labels.mainLabel.fontSize = 12;
+			pieProps.labels.percentage.fontSize = 10;
+			pieProps.labels.outer.pieDistance = 25;
+			pie = new d3pie('pieChart', pieProps);
+		} else if (screenWidth <= 420) {
+			pieProps.size.canvasHeight = 220;
+			pieProps.size.canvasWidth = 320;
+			pieProps.labels.mainLabel.fontSize = 10;
+			pieProps.labels.percentage.fontSize = 8;
+			pieProps.labels.outer.pieDistance = 15;
+			pie = new d3pie('pieChart', pieProps);
+		} else if (screenWidth > 991) {
+			pieProps.size.canvasHeight = 700;
+			pieProps.size.canvasWidth = 800;
+			pieProps.labels.mainLabel.fontSize = 16;
+			pieProps.labels.percentage.fontSize = 12;
+			pieProps.labels.outer.pieDistance = 55;
+			pie = new d3pie('pieChart', pieProps);
+		}
+	}
+	// Call once on load
+	resizePie(screen.width);
+
+	// Listen for scroll in
 	$(document).on('aos:in:pie-in', () => {
 		resizePie(screen.width);
 	});
