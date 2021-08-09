5a6e3832bef80cb56d35eb9b4921390f5b360360
Aleksandr Pasevin <pasevin@gmail.com>
2021-08-09 21:22:54 +0300

    Clean up

--- custom/js/controllers/auth.js
+++ custom/js/controllers/auth.js
@@ -64,9 +64,7 @@ export async function logIn(providerName) {
 			const params = { provider: providerName };
 			user = await Moralis.Web3.authenticate(params);
 			const web3 = await Moralis.Web3.activeWeb3Provider.activate();
-			console.log(web3);
 			currProvider = await web3.currentProvider;
-			console.log(currProvider);
 			authProvider = authenticateProvider();
 		} catch (error) {
 			console.log(error);
