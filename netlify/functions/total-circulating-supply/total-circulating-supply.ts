50f837979fced35fd92b3a701c90eecfabca234e
Aleksandr Pasevin <pasevin@gmail.com>
2021-09-15 23:01:36 +0300

    Trying to fetch and format data properly via netlify function

--- netlify/functions/total-circulating-supply/total-circulating-supply.ts
+++ netlify/functions/total-circulating-supply/total-circulating-supply.ts
@@ -1,4 +1,5 @@
 import { Handler } from '@netlify/functions';
+import fetch from 'node-fetch';
 
 const API_ENDPOINT =
 	'https://vamoxwojj7ht.moralisweb3.com:2053/server/functions/totalCirculatingSupply?_AplicationId=QfgYJskOXrYJnSAiB3KZPMMesmlJB6JBqY3GOzHV';
@@ -6,8 +7,8 @@ const API_ENDPOINT =
 export const handler: Handler = async (event, context) => {
 	try {
 		const response = await fetch(API_ENDPOINT);
-		const data = await response.json();
-		return { statusCode: 200, body: JSON.stringify({ data }) };
+		const data = await response.text();
+		return { statusCode: 200, body: data };
 	} catch (error) {
 		console.log(error);
 		return {
