fd20bd8b46204a5eba317002cd89924fafda3a12
Aleksandr Pasevin <pasevin@gmail.com>
2021-09-30 12:08:03 +0300

    Fix relative links

--- components/disclaimer/index.js
+++ components/disclaimer/index.js
@@ -1,5 +1,5 @@
 // Fetch template and update the DOM
-fetch('./components/disclaimer/template.html')
+fetch('/components/disclaimer/template.html')
 	.then((response) => response.text())
 	.then((data) => initDisclaimer(data));
 
