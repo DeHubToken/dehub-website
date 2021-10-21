bf79d819b07f52e0fc7f63b22be5dc3c45423921
Aleksandr Pasevin <pasevin@gmail.com>
2021-10-21 21:34:50 +0300

    Slider update

--- ppv/components/ppv-slider/index.js
+++ ppv/components/ppv-slider/index.js
@@ -32,9 +32,7 @@ async function initPPVSlider(data) {
 
 	// Get the data
 	const response = await client.getEntries({
-		content_type: 'ppv',
-		'fields.featured': false,
-		order: '-fields.date',
+		content_type: 'ppvSliderPost',
 	});
 	const items = response.items;
 	// console.log(items);
@@ -51,16 +49,9 @@ const addSlides = (embla, items) => {
 		$card.find('img')[0].src = f.coverImage.fields.file.url;
 		// Title
 		$card.find('.card-title').text(f.title);
-		// Date
-		const d = new Date(f.date);
-		$card.find('.date').text(d.toUTCString().replace('GMT', 'UTC'));
-		// Badge
-		const now = new Date();
-		if (d < now) {
-			$card.find('.badge.fin').toggleClass('d-none d-block');
-		} else {
-			$card.find('.badge.upcoming').toggleClass('d-none d-block');
-		}
+		// Description
+		const $converted = $(documentToHtmlString(f.description));
+		$card.find('.description').html($converted);
 		// Insert
 		$container.append($card);
 	});
@@ -69,33 +60,18 @@ const addSlides = (embla, items) => {
 };
 
 const cardTemplate = `
-	<div class="embla__slide">
-		<div class="card bg-transparent bg-gradient-2 radius10 border-0">
-			<img src="../uploads/placeholder-1920x1080.jpg"
-				class="card-img-top"
-				alt="Pay-Per-View Cover Image">
-			<div class="card-body">
-				<h6 class="card-title text-uppercase mb-5">
-					...
-				</h6>
-				<span class="date-container d-none float-left w-full mb-5">
-					<i class="fal fa-calendar-alt f-20 pr-5 pt-5"></i>&nbsp;<span class="date text-monospace small f-12 bold">...</span>
-				</span>
-				<span class="badge badge-pill badge-warning d-none float-left mb-20 mt-10 mr-5"></span>
-				<span class="badge upcoming badge-pill badge-danger d-none float-left mb-20 mt-10 mr-5">Upcoming</span>
-				<span class="badge fin badge-pill badge-secondary d-none float-left mb-20 mt-10 mr-5">Finished</span>
+	<div class="embla__slide hvr-shrink">
+		<a href="#">
+			<div class="card bg-transparent bg-gradient-2 radius10 border-0">
+				<img src="../uploads/placeholder-1920x1080.jpg"
+					class="card-img-top"
+					alt="Pay-Per-View Cover Image">
+				<div class="card-body color-main">
+					<h6 class="card-title text-uppercase mb-5">
+						...
+					</h6>
+					<div class="card-text description d-block float-left small"></div>
+				</div>
 			</div>
-			<!--
-			<div class="card-footer text-left">
-				<a href="#"
-					class="btn glass-2 shadow mb-10 text-center mr-10 w-full">
-					<span></span>
-					<span></span>
-					<span></span>
-					<span></span>
-					<span class="nonEmpty">More Info</span>
-				</a>
-			</div>
-			-->
-		</div>
+		</a>
 	</div>`;
