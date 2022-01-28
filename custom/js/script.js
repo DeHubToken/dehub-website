a4c3e0df27ea48999fd1dd540d88894b300c514b
Aleksandr Pasevin <pasevin@gmail.com>
2022-01-28 18:18:34 +0300

    fix: staking links and remove claim dapp

--- custom/js/script.js
+++ custom/js/script.js
@@ -4,62 +4,56 @@ import { initFullScreenLoader } from './full-screen-loader.js';
  */
 
 $(document).ready(function () {
-	initFullScreenLoader();
-	MY_scripts();
-	$('[data-toggle="popover"]').popover({
-		trigger: 'hover focus',
-	});
+  initFullScreenLoader();
+  MY_scripts();
+  $('[data-toggle="popover"]').popover({
+    trigger: 'hover focus',
+  });
 });
 
 function MY_scripts() {
-	console.log('MY Scripts initialized!');
-	/* ------------------------------ Click to copy ----------------------------- */
-	function copy(text) {
-		const inp = document.createElement('input');
-		document.body.appendChild(inp);
-		inp.value = text;
-		inp.select();
-		document.execCommand('copy', false);
-		inp.remove();
-	}
+  console.log('MY Scripts initialized!');
+  /* ------------------------------ Click to copy ----------------------------- */
+  function copy(text) {
+    const inp = document.createElement('input');
+    document.body.appendChild(inp);
+    inp.value = text;
+    inp.select();
+    document.execCommand('copy', false);
+    inp.remove();
+  }
 
-	let copyProcessing = false;
-	$('.clickToCopy').on('click', (e) => {
-		if (!copyProcessing) {
-			copyProcessing = true;
-			copy(e.target.innerText);
-			const orig = $(e.target).text();
-			$(e.target).text('Copied to clipboard!');
-			setTimeout(() => {
-				$(e.target).text(orig);
-				copyProcessing = false;
-			}, 3000);
-		}
-	});
+  let copyProcessing = false;
+  $('.clickToCopy').on('click', e => {
+    if (!copyProcessing) {
+      copyProcessing = true;
+      copy(e.target.innerText);
+      const orig = $(e.target).text();
+      $(e.target).text('Copied to clipboard!');
+      setTimeout(() => {
+        $(e.target).text(orig);
+        copyProcessing = false;
+      }, 3000);
+    }
+  });
 
-	/* ---------------------------- Coming soon click --------------------------- */
-	let comingSoonCoolDown = false;
-	$('.comingSoon').on('click', (e) => {
-		e.stopImmediatePropagation();
-		e.preventDefault();
-		if (!comingSoonCoolDown) {
-			comingSoonCoolDown = true;
-			const orig = $(e.target).text();
-			$(e.target).text('Coming soon!');
-			setTimeout(() => {
-				$(e.target).text(orig);
-				comingSoonCoolDown = false;
-			}, 500);
-		}
-	});
+  /* ---------------------------- Coming soon click --------------------------- */
+  let comingSoonCoolDown = false;
+  $('.comingSoon').on('click', e => {
+    e.stopImmediatePropagation();
+    e.preventDefault();
+    if (!comingSoonCoolDown) {
+      comingSoonCoolDown = true;
+      const orig = $(e.target).text();
+      $(e.target).text('Coming soon!');
+      setTimeout(() => {
+        $(e.target).text(orig);
+        comingSoonCoolDown = false;
+      }, 500);
+    }
+  });
 
-	$('.preventDefault').on('click', (e) => {
-		e.preventDefault();
-	});
-
-	$('.claim').on('click', (e) => {
-		e.preventDefault();
-		console.log('!!!!!!!!!');
-		window.open('/claim');
-	});
+  $('.preventDefault').on('click', e => {
+    e.preventDefault();
+  });
 }
